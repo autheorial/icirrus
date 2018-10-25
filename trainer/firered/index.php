8b90bbf4538848696568d6a2245eb40bfd3090b2
Alice Zhu <37034159+autheorial@users.noreply.github.com>
2018-10-25 09:57:50 -0500

    Initial commit

--- trainer/firered/index.php
+++ trainer/firered/index.php
@@ -1,1 +1,58 @@
+<?php include('../head.php');?>/ FIRE RED</title>
 
+<style type="text/css">
+body{
+	background: #DE663D;
+}
+</style>
+
+</head>
+
+<body>
+
+<div id="firered">
+
+<div id="top">
+
+<div class="container">
+
+<div id="ava"><img src="leaf.png"></div>
+<div id="meta"><h1>Fire Red <i>Gen III</i></h1>
+<h2>2011 Kanto Region</h2>
+<div class="talk"><p>Fire Red and Leaf Green didn't intrigue me as much as the newer Pok&eacute;mon games do, but one summer I was in China without internet and much else to do other than replay Pok&eacute;mon games with my brother, so I played through Fire Red. Though I like a lot of the original 150 Pok&eacute;mon, I decided to use just two on my team for maximum power and efficiency.</p></div></div>
+</div></div>
+
+<div id="pokes">
+
+<div class="pkmn">
+<img src="pidgeot.png">
+<table width="100%" class="mon"><tr><td><b>Pidgeot&#9792;</b></td> <td>Beaky <img src="../balls/pokeball.png"></td></tr>
+<tr><td><b>Lvl</b></td> <td>100</td></tr>
+<tr><td><b>Type</b></td> <td><normal>Normal</normal>/<flying>Flying</flying></td></tr>
+<tr><td><b>Ability</b></td> <td>Keen Eye</td></tr>
+<tr><td><b>Nature</b></td> <td>Impish, mischievous</td></tr>
+<tr><td><b>Ribbons</b></td> <td class="ribbons"><a class="tooltip"><img src="../ribbons/champ.png"><span class="tooltiptext">Indigo League Champion</span></a>
+</td></tr> </table>
+
+<a onclick="javascript:ShowHide('pidgeot')" class="movesetlink">Moveset</a>
+<div class="mid" id="pidgeot" style="opacity: 0"><table width="100%" class="moveset"><tr><td>Fly</td> <td>Mirror Move</td></tr> <tr><td>Quick Attack</td> <td>Aerial Ace</td></tr></table></div>
+</div>
+
+<div class="pkmn">
+<img src="primeape.png">
+<table width="100%" class="mon"><tr><td><b>Primeape&#9794;</b></td> <td>RAMBO <img src="../balls/pokeball.png"></td></tr>
+<tr><td><b>Lvl</b></td> <td>100</td></tr>
+<tr><td><b>Type</b></td> <td><fighting>Fighting</fighting></td></tr>
+<tr><td><b>Ability</b></td> <td>Vital Spirit</td></tr>
+<tr><td><b>Nature</b></td> <td>Hardy, highly curious</td></tr>
+<tr><td><b>Ribbons</b></td> <td class="ribbons"><a class="tooltip"><img src="../ribbons/champ.png"><span class="tooltiptext">Indigo League Champion</span></a>
+</td></tr> </table>
+
+<a onclick="javascript:ShowHide('primeape')" class="movesetlink">Moveset</a>
+<div class="mid" id="primeape" style="opacity: 0"><table width="100%" class="moveset"><tr><td>Poison Jab</td> <td>Earthquake</td></tr> <tr><td>Rock Slide</td> <td>Brick Break</td></tr></table></div>
+</div>
+
+
+</div></div>
+
+<?php include('../footer.php');?>
