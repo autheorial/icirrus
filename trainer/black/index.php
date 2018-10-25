d56c800f3e2a17879cb31a4fd0b702d94c390244
Alice Zhu <37034159+autheorial@users.noreply.github.com>
2018-10-25 09:34:42 -0500

    Add files via upload

--- trainer/black/index.php
+++ trainer/black/index.php
@@ -1,1 +1,104 @@
+<?php include('../head.php');?>
 
+ / BLACK</title>
+
+<style type="text/css">
+body{
+	background: #000;
+}
+</style>
+
+</head>
+
+<body>
+
+<div id="black">
+
+<div id="top">
+
+<div class="container">
+
+<div id="ava"><img src="white.png"></div>
+<div id="meta"><h1>Black <i>Gen V</i></h1>
+<h2>2011 Unova Region</h2>
+<div class="talk"><p>Pok&eacute;mon Black, as of my writing this, is the game I have the most logged hours in. Though it's been a while, I have about 390 hours of saved gameplay. This was also the first game I ever EV bred/trained for&mdash;my &hearts;, Bisharp&mdash;and filled up my <i>entire</i> Pok&eacute;dex for. So it's a lot, also one of my favorite games story-wise.</p></div></div>
+</div></div>
+
+<div id="pokes">
+
+<div class="pkmn">
+<img src="bisharp.gif">
+<table width="100%" class="mon"><tr><td><b>Bisharp&#9794;</b></td> <td>Pointy <img src="../balls/pokeball.png"></td></tr>
+<tr><td><b>Lvl</b></td> <td>100</td></tr>
+<tr><td><b>Type</b></td> <td><dark>Dark</dark>/<steel>Steel</steel></td></tr>
+<tr><td><b>Ability</b></td> <td>Defiant</td></tr>
+<tr><td><b>Nature</b></td> <td>Jolly, sturdy body</td></tr> </table>
+
+<a onclick="javascript:ShowHide('bisharp')" class="movesetlink">Moveset</a>
+<div class="mid" id="bisharp" style="opacity: 0"><table width="100%" class="moveset"><tr><td>Brick Break</td> <td>Iron Head</td></tr> <tr><td>Night Slash</td> <td>Aerial Ace</td></tr></table></div>
+</div>
+
+<div class="pkmn">
+<img src="serperior.gif">
+<table width="100%" class="mon"><tr><td><b>Serperior&#9794;</b></td> <td>Kyuhyun <img src="../balls/pokeball.png"></td></tr>
+<tr><td><b>Lvl</b></td> <td>100</td></tr>
+<tr><td><b>Type</b></td> <td><grass>Grass</grass></td></tr>
+<tr><td><b>Ability</b></td> <td>Overgrow</td></tr>
+<tr><td><b>Nature</b></td> <td>Mild, likes to thrash about</td></tr></table>
+
+<a onclick="javascript:ShowHide('serperior')" class="movesetlink">Moveset</a>
+<div class="mid" id="serperior" style="opacity: 0"><table width="100%" class="moveset"><tr><td>Leaf Blade</td> <td>Aerial Ace</td></tr> <tr><td>Slam</td> <td>Dragon Tail</td></tr></table></div>
+</div>
+
+<div class="pkmn">
+<img src="simipour.gif">
+<table width="100%" class="mon"><tr><td><b>Simipour&#9794;</b></td> <td>Hyukjae <img src="../balls/pokeball.png"></td></tr>
+<tr><td><b>Lvl</b></td> <td>100</td></tr>
+<tr><td><b>Type</b></td> <td><water>Water</water></td></tr>
+<tr><td><b>Ability</b></td> <td>Gluttony</td></tr>
+<tr><td><b>Nature</b></td> <td>Rash, good endurance</td></tr></table>
+
+<a onclick="javascript:ShowHide('simipour')" class="movesetlink">Moveset</a>
+<div class="mid" id="simipour" style="opacity: 0"><table width="100%" class="moveset"><tr><td>Dig</td> <td>Ice Beam</td></tr> <tr><td>Scald</td> <td>Acrobatics</td></tr></table></div>
+</div>
+
+<div class="pkmn">
+<img src="darmanitan.gif">
+<table width="100%" class="mon"><tr><td><b>Darmanitan&#9792;</b></td> <td>Jongwoon <img src="../balls/great.png"></td></tr>
+<tr><td><b>Lvl</b></td> <td>100</td></tr>
+<tr><td><b>Type</b></td> <td><fire>Fire</fire></td></tr>
+<tr><td><b>Ability</b></td> <td>Sheer Force</td></tr>
+<tr><td><b>Nature</b></td> <td>Lonely, highly curious</td></tr> </table>
+
+<a onclick="javascript:ShowHide('darmanitan')" class="movesetlink">Moveset</a>
+<div class="mid" id="darmanitan" style="opacity: 0"><table width="100%" class="moveset"><tr><td>Rock Slide</td> <td>Fire Punch</td></tr> <tr><td>Grass Knot</td> <td>Hammer Arm</td></tr></table></div>
+</div>
+
+<div class="pkmn">
+<img src="mienshao.gif">
+<table width="100%" class="mon"><tr><td><b>Mienshao&#9792;</b></td> <td>Jia <img src="../balls/ultra.png"></td></tr>
+<tr><td><b>Lvl</b></td> <td>100</td></tr>
+<tr><td><b>Type</b></td> <td><fighting>Fighting</fighting></td></tr>
+<tr><td><b>Ability</b></td> <td>Regenerator</td></tr>
+<tr><td><b>Nature</b></td> <td>Adamant, thoroughly cunning</td></tr></table>
+
+<a onclick="javascript:ShowHide('mienshao')" class="movesetlink">Moveset</a>
+<div class="mid" id="mienshao" style="opacity: 0"><table width="100%" class="moveset"><tr><td>U-Turn</td> <td>Aura Sphere</td></tr> <tr><td>Rock Slide</td> <td>Drain Punch</td></tr></table></div>
+</div>
+
+<div class="pkmn">
+<img src="zoroark.gif">
+<table width="100%" class="mon"><tr><td><b>Zoroark&#9794;</b></td> <td>Jaehyo <img src="../balls/pokeball.png"></td></tr>
+<tr><td><b>Lvl</b></td> <td>100</td></tr>
+<tr><td><b>Type</b></td> <td><dark>Dark</dark></td></tr>
+<tr><td><b>Ability</b></td> <td>Illusion</td></tr>
+<tr><td><b>Nature</b></td> <td>Lonely, mischievous</td></tr> </table>
+
+<a onclick="javascript:ShowHide('zoroark')" class="movesetlink">Moveset</a>
+<div class="mid" id="zoroark" style="opacity: 0"><table width="100%" class="moveset"><tr><td>Night Daze</td> <td>Nasty Plot</td></tr> <tr><td>Extrasensory</td> <td>Focus Blast</td></tr></table></div>
+</div>
+
+
+</div></div>
+
+<?php include('../footer.php');?>
