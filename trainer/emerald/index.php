ec2933c29fc272447cd69e945f1ba505071cd515
Alice Zhu <37034159+autheorial@users.noreply.github.com>
2018-10-25 09:45:38 -0500

    Add files via upload

--- trainer/emerald/index.php
+++ trainer/emerald/index.php
@@ -1,1 +1,128 @@
+<?php include('../head.php');?>/ EMERALD</title>
 
+<style type="text/css">
+body{
+	background: #3A947A;
+}
+</style>
+
+</head>
+
+<body>
+
+<div id="emerald">
+
+<div id="top">
+
+<div class="container">
+
+<div id="ava"><img src="emerald.png"></div>
+<div id="meta"><h1>Emerald <i>Gen III</i></h1>
+<h2>2006 Hoenn Region</h2>
+<div class="talk"><p>One of my most replayed games&mdash;I'd started with a bootlegged copy that kept restarting every time I stopped playing, so I'd learned for a short time to keep my attachment to my Pok&eacute;mon at the time ephemeral. Regardless, I eventually got a legitimate game and cycled through at least three significant teams when playing. The Pok&eacute;mon listed here is my one full Emerald team; the other two can be found in my "Misc" page.</p></div></div>
+</div></div>
+
+<div id="pokes">
+
+<div class="pkmn">
+<img src="sceptile.gif">
+<table width="100%" class="mon"><tr><td><b>Sceptile&#9794;</b></td> <td>Ecko <img src="../balls/pokeball.png"></td></tr>
+<tr><td><b>Lvl</b></td> <td>100</td></tr>
+<tr><td><b>Type</b></td> <td><grass>Grass</grass></td></tr>
+<tr><td><b>Ability</b></td> <td>Overgrow</td></tr>
+<tr><td><b>Nature</b></td> <td>Naive, a little quick tempered</td></tr>
+<tr><td><b>Ribbons</b></td> <td class="ribbons"><a class="tooltip"><img src="../ribbons/champ.png"><span class="tooltiptext">Elite Four Champion</span></a>
+<a class="tooltip"><a class="tooltip"><img src="../ribbons/sinnoh.png"><span class="tooltiptext">Sinnoh Champion</span></a>
+<a class="tooltip"><img src="../ribbons/effort.png"><span class="tooltiptext">Effort</span></a></td></tr> </table>
+
+<a onclick="javascript:ShowHide('sceptile')" class="movesetlink">Moveset</a>
+<div class="mid" id="sceptile" style="opacity: 0"><table width="100%" class="moveset"><tr><td>Leaf Blade</td> <td>Energy Ball</td></tr> <tr><td>Rock Slide</td> <td>Dragon Claw</td></tr></table></div>
+
+
+</div>
+
+<div class="pkmn">
+<img src="mightyena.gif">
+<table width="100%" class="mon"><tr><td><b>Mightyena&#9792;</b></td> <td>DarkFang <img src="../balls/pokeball.png"></td></tr>
+<tr><td><b>Lvl</b></td> <td>100</td></tr>
+<tr><td><b>Type</b></td> <td><dark>Dark</dark></td></tr>
+<tr><td><b>Ability</b></td> <td>Intimidate</td></tr>
+<tr><td><b>Nature</b></td> <td>Impish, likes to run</td></tr>
+<tr><td><b>Ribbons</b></td> <td class="ribbons"><a class="tooltip"><img src="../ribbons/champ.png"><span class="tooltiptext">Elite Four Champion</span></a>
+<a class="tooltip"><img src="../ribbons/sinnoh.png"><span class="tooltiptext">Sinnoh Champion</span></a><a class="tooltip"><img src="../ribbons/effort.png"><span class="tooltiptext">Effort</span></a></td></tr> </table>
+
+<a onclick="javascript:ShowHide('mightyena')" class="movesetlink">Moveset</a>
+<div class="mid" id="mightyena" style="opacity: 0;"><table width="100%" class="moveset"><tr><td>Crunch</td> <td>Sucker Punch</td></tr> <tr><td>Dig</td> <td>Return</td></tr></table></div>
+
+
+</div>
+
+<div class="pkmn">
+<img src="azumarill.gif">
+<table width="100%" class="mon"><tr><td><b>Azumarill&#9792;</b></td> <td>Aquamarine <img src="../balls/pokeball.png"></td></tr>
+<tr><td><b>Lvl</b></td> <td>100</td></tr>
+<tr><td><b>Type</b></td> <td><water>Water</water>/<fairy>Fairy</fairy></td></tr>
+<tr><td><b>Ability</b></td> <td>Thick Fat</td></tr>
+<tr><td><b>Nature</b></td> <td>Relaxed, highly persistent</td></tr>
+<tr><td><b>Ribbons</b></td> <td class="ribbons"><a class="tooltip"><img src="../ribbons/champ.png"><span class="tooltiptext">Elite Four Champion</span></a> <a class="tooltip"><img src="../ribbons/sinnoh.png"><span class="tooltiptext">Sinnoh Champion</span></a>
+</td></tr> </table>
+
+<a onclick="javascript:ShowHide('azumarill')" class="movesetlink">Moveset</a>
+<div class="mid" id="azumarill" style="opacity: 0"><table width="100%" class="moveset"><tr><td>Iron Tail</td> <td>Ice Beam</td></tr> <tr><td>Waterfall</td> <td>Brick Break</td></tr></table></div>
+
+</div>
+
+<div style="clear: both"></div>
+
+<div class="pkmn">
+<img src="torkoal.gif">
+<table width="100%" class="mon"><tr><td><b>Torkoal&#9794;</b></td> <td>FireEmblem <img src="../balls/pokeball.png"></td></tr>
+<tr><td><b>Lvl</b></td> <td>100</td></tr>
+<tr><td><b>Type</b></td> <td><fire>fire</fire></td></tr>
+<tr><td><b>Ability</b></td> <td>White Smoke</td></tr>
+<tr><td><b>Nature</b></td> <td>Gentle, highly persistent</td></tr>
+<tr><td><b>Ribbons</b></td> <td class="ribbons"><a class="tooltip"><img src="../ribbons/champ.png"><span class="tooltiptext">Elite Four Champion</span></a> <a class="tooltip"><img src="../ribbons/sinnoh.png"><span class="tooltiptext">Sinnoh Champion</span></a>
+</td></tr> </table>
+
+<a onclick="javascript:ShowHide('torkoal')" class="movesetlink">Moveset</a>
+<div class="mid" id="torkoal" style="opacity: 0"><table width="100%" class="moveset"><tr><td>Earthquake</td> <td>Flamethrower</td></tr> <tr><td>Rock Slide</td> <td>Flame Charge</td></tr></table></div>
+
+
+</div>
+
+<div class="pkmn">
+<img src="hariyama.gif">
+<table width="100%" class="mon"><tr><td><b>Hariyama&#9794;</b></td> <td>Iron Fist <img src="../balls/great.png"></td></tr>
+<tr><td><b>Lvl</b></td> <td>100</td></tr>
+<tr><td><b>Type</b></td> <td><fighting>Fighting</fighting></td></tr>
+<tr><td><b>Ability</b></td> <td>Thick Fat</td></tr>
+<tr><td><b>Nature</b></td> <td>Rash, sturdy body</td></tr>
+<tr><td><b>Ribbons</b></td> <td class="ribbons"><a class="tooltip"><img src="../ribbons/champ.png"><span class="tooltiptext">Elite Four Champion</span></a> <a class="tooltip"><img src="../ribbons/sinnoh.png"><span class="tooltiptext">Sinnoh Champion</span></a>
+<a class="tooltip"><img src="../ribbons/effort.png"><span class="tooltiptext">Effort</span></a></td></tr> </table>
+
+<a onclick="javascript:ShowHide('hariyama')" class="movesetlink">Moveset</a>
+<div class="mid" id="hariyama" style="opacity: 0"><table width="100%" class="moveset"><tr><td>Metronome</td> <td>Bulldoze</td></tr> <tr><td>Brick Break</td> <td>Rock Slide</td></tr></table></div>
+
+
+</div>
+
+<div class="pkmn">
+<img src="grumpig.gif">
+<table width="100%" class="mon"><tr><td><b>Grumpig&#9792;</b></td> <td>PearlHeart <img src="../balls/great.png"></td></tr>
+<tr><td><b>Lvl</b></td> <td>100</td></tr>
+<tr><td><b>Type</b></td> <td><psychic>Psychic</psychic></td></tr>
+<tr><td><b>Ability</b></td> <td>Own Tempo</td></tr>
+<tr><td><b>Nature</b></td> <td>Bashful, loves to eat</td></tr>
+<tr><td><b>Ribbons</b></td> <td class="ribbons"><a class="tooltip"><img src="../ribbons/champ.png"><span class="tooltiptext">Elite Four Champion</span></a> <a class="tooltip"><img src="../ribbons/sinnoh.png"><span class="tooltiptext">Sinnoh Champion</span></a>
+</td></tr> </table>
+
+<a onclick="javascript:ShowHide('grumpig')" class="movesetlink">Moveset</a>
+<div class="mid" id="grumpig" style="opacity: 0"><table width="100%" class="moveset"><tr><td>Power Gem</td> <td>Psychic</td></tr> <tr><td>Shadow Ball</td> <td>Energy Ball</td></tr></table></div>
+
+
+</div>
+
+
+</div>
+
+<?php include('../footer.php');?>
