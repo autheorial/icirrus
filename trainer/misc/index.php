35c0ba7310c3041d0d487506f9541c425f8d961d
Alice Zhu <37034159+autheorial@users.noreply.github.com>
2018-10-25 10:03:37 -0500

    Initial commit

---
+++ trainer/misc/index.php
@@ -0,0 +1,201 @@
+<?php include('../head.php');?> / MISC.</title>
+
+<style type="text/css">
+body{
+    background: #ffb83d;
+	/* background: #e7b0b6; */
+}
+
+</style>
+
+</head>
+
+<body>
+
+<div id="misc">
+
+<div id="top">
+
+<div class="container">
+
+<div id="ava"><img src="yellow.png"></div>
+<div id="meta"><h1>MISC<i>ELLANEOUS</i></h1>
+<h2>2003 - 2017</h2>
+<div class="talk">Here are all my "miscellaneous" pok&eacute;mon, aka 'mon that I've trained and loved but have never been part of an official and organic team.</p></div></div>
+</div></div>
+
+<div id="pokes">
+
+<div class="single">
+
+<div class="pkmn"><div class="mis">
+<img src="blaziken.gif">
+<table width="100%" class="mon"><tr><td><b>Blaziken&#9792;</b></td> <td>Starblaze <img src="../balls/pokeball.png"></td></tr>
+<tr><td><b>Lvl</b></td> <td>100</td></tr>
+<tr><td><b>Type</b></td> <td><fire>Fire</fire>/<fighting>Fighting</fighting></td></tr>
+<tr><td><b>Ability</b></td> <td>Blaze</td></tr>
+<tr><td><b>Nature</b></td> <td>Relaxed, impetuous and silly</td></tr> 
+<tr><td><b>Ribbons</b></td> <td class="ribbons"><a class="tooltip"><img src="../ribbons/hoenn.png"><span class="tooltiptext">Elite Four Champion</span></a> <a class="tooltip"><img src="../ribbons/sinnoh.png"><span class="tooltiptext">Sinnoh Champion</span></a> <a class="tooltip"><img src="../ribbons/effort.png"><span class="tooltiptext">Effort</span></a></td></tr>
+<tr><td><b>Game</b></td> <td><emerald>Emerald</emerald>&mdash;first runthrough; <br />only pok&eacute;mon trained</td></tr>
+</table></div>
+
+<a onclick="javascript:ShowHide('blaziken')" class="movesetlink">Moveset</a>
+<div class="mid" id="blaziken" style="opacity: 0"><table width="100%" class="moveset"><tr><td>Sky Uppercut</td> <td>Earthquake</td></tr> <tr><td>Solar Beam</td> <td>Blaze Kick</td></tr></table></div>
+</div>
+
+<div class="pkmn"><div class="mis">
+<img src="kingdra.gif">
+<table width="100%" class="mon"><tr><td><b>Kingdra&#9794;</b></td> <td>Yin Yang  <img src="../balls/ultra.png"></td></tr>
+<tr><td><b>Lvl</b></td> <td>100</td></tr>
+<tr><td><b>Type</b></td> <td><water>Water</water>/<dragon>Dragon</dragon></td></tr>
+<tr><td><b>Ability</b></td> <td>Sniper</td></tr>
+<tr><td><b>Nature</b></td> <td>Gentle, likes to thrash about</td></tr>
+<tr><td><b>Ribbons</b></td> <td class="ribbons"><a class="tooltip"><img src="../ribbons/hoenn.png"><span class="tooltiptext">Elite Four Champion</span></a> <a class="tooltip"><img src="../ribbons/sinnoh.png"><span class="tooltiptext">Sinnoh Champion</span></a> <a class="tooltip"><img src="../ribbons/footprint.png"><span class="tooltiptext">Footprint</span></a></td></tr>
+<tr><td><b>Game</b></td> <td><emerald>Emerald</emerald>&mdash;first kingdra, my<br />favorite baby <kingdra>&hearts;</kingdra></td></tr>
+</table></div>
+
+<a onclick="javascript:ShowHide('kingdra')" class="movesetlink">Moveset</a>
+<div class="mid" id="kingdra" style="opacity: 0"><table width="100%" class="moveset"><tr><td>Dragon Pulse</td> <td>Ice Beam</td></tr> <tr><td>Flash Cannon</td> <td>Surf</td></tr></table></div>
+</div>
+</div>
+
+
+<div style="clear: both"></div>
+
+<div class="group">
+
+<div class="pkmn">
+<img src="magnezone.gif">
+<table width="100%" class="mon"><tr><td><b>Magnezone</b></td> <td>Magikarp <img src="../balls/great.png"></td></tr>
+<tr><td><b>Lvl</b></td> <td>100</td></tr>
+<tr><td><b>Type</b></td> <td><electric>Electric</electric>/<steel>Steel</steel></td></tr>
+<tr><td><b>Ability</b></td> <td>Sturdy</td></tr>
+<tr><td><b>Nature</b></td> <td>Hasty, capable of taking hits</td></tr>
+<tr><td><b>Ribbons</b></td> <td class="ribbons"><a class="tooltip"><img src="../ribbons/hoenn.png"><span class="tooltiptext">Elite Four Champion</span></a></td></tr>
+<tr><td><b>Game</b></td> <td><emerald>Emerald</emerald></td></tr></table>
+
+<a onclick="javascript:ShowHide('magnezone')" class="movesetlink">Moveset</a>
+<div class="mid" id="magnezone" style="opacity: 0"><table width="100%" class="moveset"><tr><td>Thunderbolt</td> <td>Magnet Rise</td></tr> <tr><td>Charge Beam</td> <td>Flash Cannon</td></tr></table></div>
+</div>
+
+
+<div class="pkmn">
+<img src="swampert.gif">
+<table width="100%" class="mon"><tr><td><b>Swampert&#9794;</b></td> <td>RAMBO <img src="../balls/pokeball.png"></td></tr>
+<tr><td><b>Lvl</b></td> <td>100</td></tr>
+<tr><td><b>Type</b></td> <td><water>Water</water>/<ground>Ground</ground></td></tr>
+<tr><td><b>Ability</b></td> <td>Torrent</td></tr>
+<tr><td><b>Nature</b></td> <td>Gentle, impetuous and silly</td></tr>
+<tr><td><b>Ribbons</b></td> <td class="ribbons"><a class="tooltip"><img src="../ribbons/hoenn.png"><span class="tooltiptext">Elite Four Champion</span></a></td></tr>
+<tr><td><b>Game</b></td> <td><emerald>Emerald</emerald></td></tr></table>
+
+<a onclick="javascript:ShowHide('swampert')" class="movesetlink">Moveset</a>
+<div class="mid" id="swampert" style="opacity: 0"><table width="100%" class="moveset"><tr><td>Earthquake</td> <td>Brick Break</td></tr> <tr><td>Ice Beam</td> <td>Surf</td></tr></table></div>
+</div>
+
+<div class="pkmn">
+<img src="crobat.gif">
+<table width="100%" class="mon"><tr><td><b>Crobat&#9792;</b></td> <td>Zoo <img src="../balls/pokeball.png"></td></tr>
+<tr><td><b>Lvl</b></td> <td>100</td></tr>
+<tr><td><b>Type</b></td> <td><poison>Poison</poison>/<flying>Flying</flying></td></tr>
+<tr><td><b>Ability</b></td> <td>Inner Focus</td></tr>
+<tr><td><b>Nature</b></td> <td>Naughty, somewhat vain</td></tr>
+<tr><td><b>Ribbons</b></td> <td class="ribbons"><a class="tooltip"><img src="../ribbons/hoenn.png"><span class="tooltiptext">Elite Four Champion</span></a></td></tr>
+<tr><td><b>Game</b></td> <td><emerald>Emerald</emerald></td></tr></table>
+
+<a onclick="javascript:ShowHide('crobat')" class="movesetlink">Moveset</a>
+<div class="mid" id="crobat" style="opacity: 0"><table width="100%" class="moveset"><tr><td>Bite</td> <td>Aerial Ace</td></tr> <tr><td>Poison Fang</td> <td>Fly</td></tr></table></div>
+</div>
+
+</div>
+
+<div style="clear: both"></div>
+
+
+<div class="single">
+
+<div class="pkmn"><div class="mis">
+<img src="ambipom.gif">
+<table width="100%" class="mon"><tr><td><b>Ambipom&#9792;</b></td> <td>Pinky <img src="../balls/safari.png"></td></tr>
+<tr><td><b>Lvl</b></td> <td>100</td></tr>
+<tr><td><b>Type</b></td> <td><normal>Normal</normal></td></tr>
+<tr><td><b>Ability</b></td> <td>Pickup</td></tr>
+<tr><td><b>Nature</b></td> <td>Relaxed, impetuous and silly</td></tr> 
+<tr><td><b>Ribbons</b></td> <td class="ribbons"><a class="tooltip"><img src="../ribbons/hoenn.png"><span class="tooltiptext">Elite Four Champion</span></a></td></tr>
+<tr><td><b>Game</b></td> <td><emerald>Emerald</emerald>&mdash;random Safari <br />encounter (I got attached<br /><pink>&hearts;</pink>)</td></tr>
+</table></div>
+
+<a onclick="javascript:ShowHide('ambipom')" class="movesetlink">Moveset</a>
+<div class="mid" id="ambipom" style="opacity: 0"><table width="100%" class="moveset"><tr><td>Baton Pass</td> <td>Brick Break</td></tr> <tr><td>Shadow Claw</td> <td>Double Hit</td></tr></table></div>
+</div>
+
+<div class="pkmn"><div class="mis">
+<img src="furret.gif">
+<table width="100%" class="mon"><tr><td><b>Furret&#9792;</b></td> <td>Alice <img src="../balls/great.png"></td></tr>
+<tr><td><b>Lvl</b></td> <td>100</td></tr>
+<tr><td><b>Type</b></td> <td><normal>Normal</normal></td></tr>
+<tr><td><b>Ability</b></td> <td>Keen Eye</td></tr>
+<tr><td><b>Nature</b></td> <td>Calm, proud of its power</td></tr>
+<tr><td><b>Ribbons</b></td> <td class="ribbons"><a class="tooltip"><img src="../ribbons/hoenn.png"><span class="tooltiptext">Elite Four Champion</span></a> <a class="tooltip"><img src="../ribbons/sinnoh.png"><span class="tooltiptext">Sinnoh Champion</span></a> <a class="tooltip"><img src="../ribbons/shock.png"><span class="tooltiptext">Shock</span></a></td></tr>
+<tr><td><b>Game</b></td> <td>A gift from my brother on his <leaf>LeafGreen</leaf> game</td></tr>
+</table></div>
+
+<a onclick="javascript:ShowHide('furret')" class="movesetlink">Moveset</a>
+<div class="mid" id="furret" style="opacity: 0"><table width="100%" class="moveset"><tr><td>Iron Tail</td> <td>Dig</td></tr> <tr><td>Shadow Claw</td> <td>Flamethrower</td></tr></table></div>
+</div>
+</div>
+
+<div style="clear: both"></div>
+
+<div class="group">
+
+<div class="pkmn">
+<img src="kangaskhan.gif">
+<table width="100%" class="mon"><tr><td><b>Kangaskhan&#9792;</b></td> <td><img src="../balls/dream.png"></td></tr>
+<tr><td><b>Lvl</b></td> <td>55</td></tr>
+<tr><td><b>Type</b></td> <td><normal>Normal</normal></td></tr>
+<tr><td><b>Ability</b></td> <td>Scrappy/Parental Bond</td></tr>
+<tr><td><b>Nature</b></td> <td>Jolly, somewhat stubborn</td></tr>
+<tr><td><b>Ribbons</b></td> <td class="ribbons"><a class="tooltip"><img src="../ribbons/skillful.png"><span class="tooltiptext">Skillful</span></a><a class="tooltip"><img src="../ribbons/expert.png"><span class="tooltiptext">Battle Expert</span></a><a class="tooltip"><img src="../ribbons/footprint.png"><span class="tooltiptext">Footprint</span></a></td></tr>
+<tr><td><b>Game</b></td> <td><omegaruby>Omega Ruby</omegaruby>&mdash;Super Singles first 50-streak win with Kangaskhanite</td></tr></table>
+
+<a onclick="javascript:ShowHide('megakhan')" class="movesetlink">Moveset</a>
+<div class="mid" id="megakhan" style="opacity: 0"><table width="100%" class="moveset"><tr><td>Power-Up Punch</td> <td>Return</td></tr> <tr><td>Sucker Punch</td> <td>Fake Out</td></tr></table></div>
+</div>
+
+
+<div class="pkmn">
+<img src="aegislash.gif">
+<table width="100%" class="mon"><tr><td><b>Aegislash&#9792;</b></td> <td><img src="../balls/pokeball.png"></td></tr>
+<tr><td><b>Lvl</b></td> <td>55</td></tr>
+<tr><td><b>Type</b></td> <td><ghost>Ghost</ghost>/<steel>Steel</steel></td></tr>
+<tr><td><b>Ability</b></td> <td>Stance Change</td></tr>
+<tr><td><b>Nature</b></td> <td>Quiet, very finicky</td></tr>
+<tr><td><b>Ribbons</b></td> <td class="ribbons"><a class="tooltip"><img src="../ribbons/expert.png"><span class="tooltiptext">Battle Expert</span></a></td></tr>
+<tr><td><b>Game</b></td> <td><omegaruby>Omega Ruby</omegaruby>&mdash;Super Singles first 50-streak win with Weakness Policy</td></tr></table>
+<a onclick="javascript:ShowHide('aegislash')" class="movesetlink">Moveset</a>
+<div class="mid" id="aegislash" style="opacity: 0"><table width="100%" class="moveset"><tr><td>Flash Cannon</td> <td>Shadow Ball</td></tr> <tr><td>Shadow Sneak</td> <td>King's Shield</td></tr></table></div>
+</div>
+
+<div class="pkmn">
+<img src="greninja.gif">
+<table width="100%" class="mon"><tr><td><b>Greninja&#9794;</b></td> <td><img src="../balls/pokeball.png"></td></tr>
+<tr><td><b>Lvl</b></td> <td>52</td></tr>
+<tr><td><b>Type</b></td> <td><water>Water</water>/<dark>Dark</dark></td></tr>
+<tr><td><b>Ability</b></td> <td>Protean</td></tr>
+<tr><td><b>Nature</b></td> <td>Naive, very finicky</td></tr>
+<tr><td><b>Ribbons</b></td> <td class="ribbons"><a class="tooltip"><img src="../ribbons/hoenn.png"><span class="tooltiptext">Elite Four Champion</span></a> <a class="tooltip"><img src="../ribbons/expert.png"><span class="tooltiptext">Battle Expert</span></a></td></tr>
+<tr><td><b>Game</b></td> <td><omegaruby>Omega Ruby</omegaruby>&mdash;Super Singles first 50-streak win with Focus Sash </td></tr></table>
+
+<a onclick="javascript:ShowHide('greninja')" class="movesetlink">Moveset</a>
+<div class="mid" id="greninja" style="opacity: 0"><table width="100%" class="moveset"><tr><td>Ice Beam</td> <td>Scald</td></tr> <tr><td>Dark Pulse</td> <td>Gunk Shot</td></tr></table></div>
+</div>
+
+</div>
+
+
+</div>
+
+</div>
+
+<?php include('../footer.php');?>
