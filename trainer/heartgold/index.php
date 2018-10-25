402246528d65833c233a6610172641eab69a5cb4
Alice Zhu <37034159+autheorial@users.noreply.github.com>
2018-10-25 10:02:42 -0500

    Initial commit

--- trainer/heartgold/index.php
+++ trainer/heartgold/index.php
@@ -1,1 +1,76 @@
+<?php include('../head.php');?> / HEART GOLD</title>
 
+<style type="text/css">
+body{
+	background: #e5c974;
+}
+
+</style>
+
+</head>
+
+<body>
+
+<div id="heartgold">
+
+<div id="top">
+
+<div class="container">
+
+<div id="ava"><img src="lyra.png"></div>
+<div id="meta"><h1>Heart Gold <i>Gen IV</i></h1>
+<h2>2012 Johto Region</h2>
+<div class="talk"><p>I was actually pretty disappointed in myself when I didn't get HGSS when it came out, so in 2012 I decided to buy a used copy. I restarted right away, which was a mistake, since I didn't transfer the Pok&eacute;mon that the previous owner had on it... the rare and exclusive event Pok&eacute;mon, including the special pichu. Oops.</p> <p>Anyway, this was my first ever Johto game <strike>after over 10 years</strike> and I adore it.</p></div></div>
+</div></div>
+
+<div id="pokes">
+
+<div class="pkmn">
+<img src="meganium.png">
+<table width="100%" class="mon"><tr><td><b>Meganium&#9794;</b></td> <td>Taffy <img src="../balls/pokeball.png"></td></tr>
+<tr><td><b>Lvl</b></td> <td>100</td></tr>
+<tr><td><b>Type</b></td> <td><grass>Grass</grass></td></tr>
+<tr><td><b>Ability</b></td> <td>Overgrow</td></tr>
+<tr><td><b>Nature</b></td> <td>Careful, often lost in thought</td></tr> 
+<tr><td><b>Ribbons</b></td> <td class="ribbons"><a class="tooltip"><img src="../ribbons/hoenn.png"><span class="tooltiptext">Elite Four Champion</span></a>
+ <a class="tooltip"><img src="../ribbons/legend.png"><span class="tooltiptext">Legend</span></a>
+</td></tr></table>
+
+<a onclick="javascript:ShowHide('meganium')" class="movesetlink">Moveset</a>
+<div class="mid" id="meganium" style="opacity: 0"><table width="100%" class="moveset"><tr><td>Light Screen</td> <td>Magical Leaf</td></tr> <tr><td>Razor Leaf</td> <td>Body Slam</td></tr></table></div>
+</div>
+
+<div class="pkmn">
+<img src="arcanine.png">
+<table width="100%" class="mon"><tr><td><b>Arcanine&#9794;</b></td> <td>Xing <img src="../balls/pokeball.png"></td></tr>
+<tr><td><b>Lvl</b></td> <td>100</td></tr>
+<tr><td><b>Type</b></td> <td><fire>Fire</fire></td></tr>
+<tr><td><b>Ability</b></td> <td>Intimidate</td></tr>
+<tr><td><b>Nature</b></td> <td>Modest, likes to thrash about</td></tr>
+<tr><td><b>Ribbons</b></td> <td class="ribbons"><a class="tooltip"><img src="../ribbons/hoenn.png"><span class="tooltiptext">Elite Four Champion</span></a>
+ <a class="tooltip"><img src="../ribbons/effort.png"><span class="tooltiptext">Effort</span></a>
+ <a class="tooltip"><img src="../ribbons/legend.png"><span class="tooltiptext">Legend</span></a>
+</td></tr></table>
+
+<a onclick="javascript:ShowHide('arcanine')" class="movesetlink">Moveset</a>
+<div class="mid" id="arcanine" style="opacity: 0"><table width="100%" class="moveset"><tr><td>Flamethrower</td> <td>Thunder Fang</td></tr> <tr><td>Dragon Pulse</td> <td>Crunch</td></tr></table></div>
+</div>
+
+<div class="pkmn">
+<img src="poliwrath.png">
+<table width="100%" class="mon"><tr><td><b>Poliwrath&#9794;</b></td> <td>Swaggy <img src="../balls/pokeball.png"></td></tr>
+<tr><td><b>Lvl</b></td> <td>100</td></tr>
+<tr><td><b>Type</b></td> <td><water>Water</water>/<fighting>Fighting</fighting></td></tr>
+<tr><td><b>Ability</b></td> <td>Damp</td></tr>
+<tr><td><b>Nature</b></td> <td>Quiet, likes to relax</td></tr>
+<tr><td><b>Ribbons</b></td> <td class="ribbons"><a class="tooltip"><img src="../ribbons/hoenn.png"><span class="tooltiptext">Elite Four Champion</span></a>
+ <a class="tooltip"><img src="../ribbons/legend.png"><span class="tooltiptext">Legend</span></a>
+</td></tr></table>
+
+<a onclick="javascript:ShowHide('poliwrath')" class="movesetlink">Moveset</a>
+<div class="mid" id="poliwrath" style="opacity: 0"><table width="100%" class="moveset"><tr><td>Earthquake</td> <td>Ice Beam</td></tr> <tr><td>Waterfall</td> <td>Brick Break</td></tr></table></div>
+</div>
+
+</div></div>
+
+<?php include('../footer.php');?>
