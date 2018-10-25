83e5177b51b2a324f00e07e9e22db8aa56f02090
Alice Zhu <37034159+autheorial@users.noreply.github.com>
2018-10-25 09:41:27 -0500

    Add files via upload

--- trainer/diamond/index.php
+++ trainer/diamond/index.php
@@ -1,1 +1,99 @@
+<?php include('../head.php');?> / BLACK 2</title>
 
+<style type="text/css">
+body{
+	background: #fff;
+	color: #444!important;
+}
+
+.talk, #footer a{
+	color: #444;
+}
+
+#footer a:hover{
+	color: #fff!important;
+}
+
+</style>
+
+</head>
+
+<body>
+
+<div id="black2">
+
+<div id="top">
+
+<div class="container">
+
+<div id="ava"><img src="rosa.png"></div>
+<div id="meta"><h1>Black 2 <i>Gen V</i></h1>
+<h2>2012 Unova Region</h2>
+<div class="talk"><p>The most important thing about my playing Pok&eacute;mon Black 2 is that I spent 12 hours looking for and catching a female eevee.</p></div></div>
+</div></div>
+
+<div id="pokes">
+
+<div class="pkmn">
+<img src="flareon.gif">
+<table width="100%" class="mon"><tr><td><b>Flareon&#9792;</b></td> <td>Kimiko <img src="../balls/pokeball.png"></td></tr>
+<tr><td><b>Lvl</b></td> <td>100</td></tr>
+<tr><td><b>Type</b></td> <td><fire>Fire</fire></td></tr>
+<tr><td><b>Ability</b></td> <td>Flash Fire</td></tr>
+<tr><td><b>Nature</b></td> <td>Bold, takes plenty of siestas</td></tr> </table>
+
+<a onclick="javascript:ShowHide('flareon')" class="movesetlink">Moveset</a>
+<div class="mid" id="flareon" style="opacity: 0"><table width="100%" class="moveset"><tr><td>Lava Plume</td> <td>Fire Fang</td></tr> <tr><td>Quick Attack</td> <td>Bite</td></tr></table></div>
+</div>
+
+<div class="pkmn">
+<img src="excadrill.gif">
+<table width="100%" class="mon"><tr><td><b>Excadrill&#9794;</b></td> <td>Clay  <img src="../balls/great.png"></td></tr>
+<tr><td><b>Lvl</b></td> <td>100</td></tr>
+<tr><td><b>Type</b></td> <td><ground>Ground</ground>/<steel>Steel</steel></td></tr>
+<tr><td><b>Ability</b></td> <td>Sand Rush</td></tr>
+<tr><td><b>Nature</b></td> <td>Mild, strong-willed</td></tr></table>
+
+<a onclick="javascript:ShowHide('excadrill')" class="movesetlink">Moveset</a>
+<div class="mid" id="excadrill" style="opacity: 0"><table width="100%" class="moveset"><tr><td>Earthquake</td> <td>Rock Slide</td></tr> <tr><td>Metal Claw</td> <td>Sandstorm</td></tr></table></div>
+</div>
+
+<div class="pkmn">
+<img src="samurott.gif">
+<table width="100%" class="mon"><tr><td><b>Samurott&#9794;</b></td> <td>Omi <img src="../balls/pokeball.png"></td></tr>
+<tr><td><b>Lvl</b></td> <td>100</td></tr>
+<tr><td><b>Type</b></td> <td><water>Water</water></td></tr>
+<tr><td><b>Ability</b></td> <td>Torrent</td></tr>
+<tr><td><b>Nature</b></td> <td>Careful, quick-tempered</td></tr></table>
+
+<a onclick="javascript:ShowHide('samurott')" class="movesetlink">Moveset</a>
+<div class="mid" id="samurott" style="opacity: 0"><table width="100%" class="moveset"><tr><td>Ice Beam</td> <td>Surf</td></tr> <tr><td>Scald</td> <td>Hidden Power</td></tr></table></div>
+</div>
+
+<div class="pkmn">
+<img src="lucario.gif">
+<table width="100%" class="mon"><tr><td><b>Lucario&#9794;</b></td> <td>Chase <img src="../balls/pokeball.png"></td></tr>
+<tr><td><b>Lvl</b></td> <td>95</td></tr>
+<tr><td><b>Type</b></td> <td><fighting>Fighting</fighting>/<steel>Steel</steel></td></tr>
+<tr><td><b>Ability</b></td> <td>Steadfast</td></tr>
+<tr><td><b>Nature</b></td> <td>Naive, strong willed</td></tr> </table>
+
+<a onclick="javascript:ShowHide('lucario')" class="movesetlink">Moveset</a>
+<div class="mid" id="lucario" style="opacity: 0"><table width="100%" class="moveset"><tr><td>Force Palm</td> <td>Rock Smash</td></tr> <tr><td>Aura Sphere</td> <td>Bone Rush</td></tr></table></div>
+</div>
+
+<div class="pkmn">
+<img src="braviary.gif">
+<table width="100%" class="mon"><tr><td><b>Braviary&#9794;</b></td> <td>Raimundo <img src="../balls/great.png"></td></tr>
+<tr><td><b>Lvl</b></td> <td>75</td></tr>
+<tr><td><b>Type</b></td> <td><normal>Normal</normal>/<flying>Flying</flying></td></tr>
+<tr><td><b>Ability</b></td> <td>Keen Eye</td></tr>
+<tr><td><b>Nature</b></td> <td>Jolly, loves to eat</td></tr></table>
+
+<a onclick="javascript:ShowHide('braviary')" class="movesetlink">Moveset</a>
+<div class="mid" id="braviary" style="opacity: 0"><table width="100%" class="moveset"><tr><td>Slash</td> <td>Fly</td></tr> <tr><td>Tailwind</td> <td>Air Slash</td></tr></table></div>
+</div>
+
+</div></div>
+
+<?php include('../footer.php');?>
